e66de7b4b1539f5d5265ad8b99c1b2f90a227269
fabiomarcato <fabiosm@gmail.com>
2021-08-10 19:59:18 -0300

    Refatoração String String/FormatPhoneNumber.js

--- String/FormatPhoneNumber.js
+++ String/FormatPhoneNumber.js
@@ -1,14 +1,14 @@
 // function that takes 10 digits and returns a string of the formatted phone number
-// e.g.: 1234567890 -> (123) 456-7890
+// e.g.: 1234567890 -> (12) 3456-7890
 
-const formatPhoneNumber = (numbers) => {
-  const numbersString = numbers.toString()
-  if ((numbersString.length !== 10) || isNaN(numbersString)) {
+const formatPhoneNumber = (phoneNumbers) => {
+  const phoneNumbersString = phoneNumbers.toString()
+  if ((phoneNumbersString.length !== 10) || isNaN(phoneNumbersString)) {
     // return "Invalid phone number."
     throw new TypeError('Invalid phone number.')
   }
-  const arr = '(XXX) XXX-XXXX'.split('')
-  Array.from(numbersString).forEach(n => {
+  const arr = '(XX) XXXX-XXXX'.split('')
+  Array.from(phoneNumbersString).forEach(n => {
     arr[arr.indexOf('X')] = n
   })
   return arr.join('')
