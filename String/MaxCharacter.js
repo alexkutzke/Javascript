cae528d28f33e30b31ad193d630caa8403578959
Jorge Hernandesm <jorgehernandes88@gmail.com>
2021-08-11 22:43:13 -0300

    Commit Trabalho - Jorge Hernandes

--- String/MaxCharacter.js
+++ String/MaxCharacter.js
@@ -2,16 +2,11 @@
   Given a string of characters, return the character that appears the most often.
   Example: input = "Hello World!" return "l"
 */
-const maxCharacter = (value) => {
-  if (typeof value !== 'string') {
-    throw new TypeError('The param should be a string')
-  } else if (!value) {
-    throw new Error('The param should be a valid string')
-  }
-
+const letterThatAppearsTheMost = (WordForVerification) => {
+	
   const occurrences = {}
-  for (let i = 0; i < value.length; i++) {
-    const char = value[i]
+  for (let i = 0; i < WordForVerification.length; i++) {
+    const char = WordForVerification[i]
     if (/\s/.test(char)) continue
     occurrences[char] = occurrences[char] + 1 || 1
   }
@@ -24,6 +19,16 @@ const maxCharacter = (value) => {
     }
   })
   return maxCharacter
+}
+
+const maxCharacter = (Word) => {
+  if (typeof Word !== 'string') {
+    throw new TypeError('The param should be a string')
+  } else if (!Word) {
+    throw new Error('The param should be a valid string')
+  }
+
+  return letterThatAppearsTheMost(Word)
 }
 
 export { maxCharacter }
