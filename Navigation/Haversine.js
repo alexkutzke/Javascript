6ed1b7390f863244c00c695f7fafd1c326128f30
vanessa volochen <vanessa.volochen@gmail.com>
2021-08-12 18:15:57 -0300

    Refactor

--- Navigation/Haversine.js
+++ Navigation/Haversine.js
@@ -6,27 +6,41 @@
   @Param {number} longitude1
   @Param {number} longitude2
  */
-const haversineDistance = (latitude1 = 0, longitude1 = 0, latitude2 = 0, longitude2 = 0) => {
-  validateLatOrLong(latitude1)
-  validateLatOrLong(latitude2)
-  validateLatOrLong(longitude1)
-  validateLatOrLong(longitude2)
-  const earthRadius = 6371e3 // 6,371km
   const pi = Math.PI
-  const cos1 = latitude1 * pi / 180.0
-  const cos2 = latitude2 * pi / 180.0
-  const deltaLatitude = (latitude2 - latitude1) * pi / 180.0
-  const deltaLongitude = (longitude2 - longitude1) * pi / 180.0
 
-  const alpha = Math.sin(deltaLatitude / 2) * Math.sin(deltaLatitude / 2) + Math.cos(cos1) * Math.cos(cos2) * Math.sin(deltaLongitude / 2) * Math.sin(deltaLongitude / 2)
-  const constant = 2 * Math.atan2(Math.sqrt(alpha), Math.sqrt(1 - alpha))
-  return earthRadius * constant
-}
-
-const validateLatOrLong = value => {
-  if (typeof value !== 'number') {
-    throw new TypeError('The value of latitude or longitude should be a number')
+  const haversineDistance = (latitude1 = 0, longitude1 = 0, latitude2 = 0, longitude2 = 0) => {
+    validateAllLatitudeLongitude(latitude1, longitude1, latitude2, longitude2)
+  
+    const earthRadius = 6371e3 // 6,371km
+    const cos1 = calculateCosine(latitude1)
+    const cos2 = calculateCosine(latitude2)
+    const deltaLatitude = calculateDistanceDelta(latitude2, latitude1)
+    const deltaLongitude = calculateDistanceDelta(longitude2, longitude1)
+  
+    const alpha = Math.sin(deltaLatitude / 2) * Math.sin(deltaLatitude / 2) + Math.cos(cos1) * Math.cos(cos2) * Math.sin(deltaLongitude / 2) * Math.sin(deltaLongitude / 2)
+    const constant = 2 * Math.atan2(Math.sqrt(alpha), Math.sqrt(1 - alpha))
+    return earthRadius * constant
   }
-}
-
-export { haversineDistance }
+  
+  const validateAllLatitudeLongitude = (latitude1, longitude1, latitude2, longitude2) => {
+    validateLatOrLong(latitude1)
+    validateLatOrLong(latitude2)
+    validateLatOrLong(longitude1)
+    validateLatOrLong(longitude2)
+  };
+  
+  const validateLatOrLong = value => {
+    if (typeof value !== 'number') {
+      throw new TypeError('The value of latitude or longitude should be a number')
+    }
+  }
+  
+  const calculateCosine = latitude => {
+    return latitude * pi / 180.0
+  }
+  
+  const calculateDistanceDelta = (distancePoint2, distancePoint1) => {
+    return (distancePoint2 - distancePoint1) * pi / 180.0
+  }
+  
+  export { haversineDistance }
