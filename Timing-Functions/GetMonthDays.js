5baa3a85d4339f104e64b45fb96c6cc9c43d810b
Gustavo Angelo da Silva <gustavosilvaangelo@gmail.com>
2021-08-13 23:42:11 -0300

    GetMonthDays update - Gustavo Angelo da Silva

--- Timing-Functions/GetMonthDays.js
+++ Timing-Functions/GetMonthDays.js
@@ -6,28 +6,24 @@
   e.g.: mahfoudh.arous.com ->false
 */
 
+const the31DaysMonths = [1, 3, 5, 7, 8, 10, 12];
+const the30DaysMonths = [4, 6, 9, 11];
+
+const validMonths = [...the31DaysMonths, ...the30DaysMonths, ...[2]];
+
+const isValidMonth = (monthNumber) => {
+  return validMonths.includes(monthNumber);
+}
+
+const isLeapYear = (year) => {
+  return ((year % 400 === 0) || ((year % 4 === 0) && (year % 100 !== 0)))
+}
+ 
 const getMonthDays = (monthNumber, year) => {
-  const the31DaysMonths = [1, 3, 5, 7, 8, 10, 12]
-  const the30DaysMonths = [4, 6, 9, 11]
-
-  if (!the31DaysMonths.includes(monthNumber) && !the30DaysMonths.includes(monthNumber) &&
-    (monthNumber !== 2)
-  ) {
-    throw new TypeError('Invalid Month Number.')
-  }
-
-  if (the31DaysMonths.includes(monthNumber)) { return 31 }
-
-  if (the30DaysMonths.includes(monthNumber)) { return 30 }
-
-  // Check for Leap year
-  if (year % 4 === 0) {
-    if ((year % 100 !== 0) || (year % 100 === 0 && year % 400 === 0)) {
-      return 29
-    }
-  }
-
-  return 28
+  if(!isValidMonth(monthNumber))
+    throw new TypeError('Invalid Month Number.');  
+  
+  return the31DaysMonths.includes(monthNumber) ? 31 : (the30DaysMonths.includes(monthNumber) ? 30 : (isLeapYear(year) ? 29 : 28));  
 }
 
 export { getMonthDays }
