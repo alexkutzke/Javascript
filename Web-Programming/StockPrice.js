2c15cec259ebb0e9abe95d59be38a34814167690
Daniel Versoza <60162544+dversoza@users.noreply.github.com>
2021-08-14 09:41:22 -0300

    Refatoração da função getStockPrice

--- Web-Programming/StockPrice.js
+++ Web-Programming/StockPrice.js
@@ -1,29 +1,41 @@
 const fetch = require('node-fetch')
 const jsdom = require('jsdom')
 
-// function to get the stock price from the given symbol
-async function getStockPrice (stockSymbol) {
-  // parsing the html page body
-  const url = `https://in.finance.yahoo.com/lookup?s=$${stockSymbol}`
+function createStockUrl(stockSymbol) {
+  return `https://in.finance.yahoo.com/lookup?s=$${stockSymbol}`
+}
+
+async function fetchPageDomAsJson(url) {
   const response = await fetch(url)
   const pageBody = await response.text()
-  const dom = new jsdom.JSDOM(pageBody, 'text/html')
-  // returning the price as a number
-  return parseFloat(dom.window.document.querySelectorAll('td')[2].textContent.replace(/,/g, ''))
+  return new jsdom.JSDOM(pageBody, 'text/html')
+}
+
+function findStockPriceInDom(dom) {
+  const stockString = dom.window.document.querySelectorAll('td')[2].textContent.replace(/,/g, '')
+  return parseFloat(stockString)
+}
+
+async function getStockPriceFrom(stockSymbol) {
+  const url = createStockUrl(stockSymbol)
+  const domJsonObject = await fetchPageDomAsJson(url)
+  const stockPrice = findStockPriceInDom(domJsonObject)
+  
+  return stockPrice
 }
 
 async function main () {
   // Using async await to ensure synchronous behaviour
-  await getStockPrice('GOOGL')
+  await getStockPriceFrom('GOOGL')
     .then(response => console.log(`GOOGL stock price: $ ${response}`))
 
-  await getStockPrice('AAPL')
+  await getStockPriceFrom('AAPL')
     .then(response => console.log(`AAPL stock price: $ ${response}`))
 
-  await getStockPrice('MSFT')
+  await getStockPriceFrom('MSFT')
     .then(response => console.log(`MSFT stock price: $ ${response}`))
 
-  await getStockPrice('AMZN')
+  await getStockPriceFrom('AMZN')
     .then(response => console.log(`AMZN stock price: $ ${response}`))
 }
 
