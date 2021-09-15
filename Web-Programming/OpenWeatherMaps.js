324aa317b23bd25bf88f3e8966399a175525dc14
Bruno <brunocmpr@gmail.com>
2021-09-14 22:57:08 -0300

    Adequa código a práticas clean code para métodos e nomeação de objetos

--- Web-Programming/OpenWeatherMaps.js
+++ Web-Programming/OpenWeatherMaps.js
@@ -3,29 +3,44 @@ const fetch = require('node-fetch')
 const APPID = '' // <-- Put your OpenWeatherMap appid here!
 const URL_BASE = 'http://api.openweathermap.org/data/2.5/'
 
-async function currentWeather (location) {
-  const response = await fetch(`${URL_BASE}weather?q=${location}&appid=${APPID}`)
-  const data = await response.json()
-  return data
+async function fetchWeather (locationName){
+  const apiResponse = await fetch(`${URL_BASE}weather?q=${locationName}&appid=${APPID}`)
+  return apiResponse
 }
 
-async function weatherForecast (location) {
-  const response = await fetch(`${URL_BASE}forecast?q=${location}&appid=${APPID}`)
-  const data = await response.json()
-  return data
+async function fetchForecast (locationName){
+  const apiResponse = await fetch(`${URL_BASE}forecast?q=${locationName}&appid=${APPID}`)
+  return apiResponse
 }
 
-async function oneCallApi (latitude, longitude) {
-  const response = await fetch(`${URL_BASE}onecall?lat=${latitude}&lon=${longitude}&appid=${APPID}`)
-  const data = await response.json()
-  return data
+async function fetchOneCallAPI (latitude, longitude) {
+  const apiResponse = await fetch(`${URL_BASE}onecall?lat=${latitude}&lon=${longitude}&appid=${APPID}`)
+  return apiResponse
 }
 
-currentWeather('Kolkata')
-  .then(data => console.log(data))
+async function requestCurrentWeather (locationName) {
+  const apiResponse = fetchWeather(locationName)
+  const responseJson = await apiResponse.json()
+  return responseJson
+}
 
-weatherForecast('Kolkata')
-  .then(data => console.log(data))
+async function requestWeatherForecast (locationName) {
+  const apiResponse = fetchForecast (locationName)
+  const responseJson = await apiResponse.json()
+  return responseJson
+}
 
-oneCallApi(55.68, 12.57)
-  .then(data => console.log(data))
+async function requestOneCallApi (latitude, longitude) {
+  const apiResponse = fetchOneCallAPI (latitude, longitude)
+  const responseJson = await apiResponse.json()
+  return responseJson
+}
+
+requestCurrentWeather('Kolkata')
+  .then(weatherData => console.log(weatherData))
+
+requestWeatherForecast('Kolkata')
+  .then(forecastData => console.log(forecastData))
+
+requestOneCallApi(55.68, 12.57)
+  .then(oneCallApiData => console.log(oneCallApiData))
