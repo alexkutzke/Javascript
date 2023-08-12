5c86ff9c96dead7d76ab793f6505b403cfe37d47
Maria Eduarda Franke Grácia Pereira <77688432+dudafranke@users.noreply.github.com>
2023-08-12 16:35:49 -0300

    Refactoring GetMonthDays.js

--- Timing-Functions/GetMonthDays.js
+++ Timing-Functions/GetMonthDays.js
@@ -7,27 +7,38 @@
 */
 
 const getMonthDays = (monthNumber, year) => {
-  const the31DaysMonths = [1, 3, 5, 7, 8, 10, 12]
-  const the30DaysMonths = [4, 6, 9, 11]
+  isValidMonth(monthNumber)
+  return calculateMonthDays(monthNumber, year)
+}
 
-  if (!the31DaysMonths.includes(monthNumber) && !the30DaysMonths.includes(monthNumber) &&
-    (monthNumber !== 2)
-  ) {
+const isValidMonth = monthNumber => {
+  if (!Number.isInteger(monthNumber) || monthNumber < 1 || monthNumber > 12) {
     throw new TypeError('Invalid Month Number.')
   }
+}
 
-  if (the31DaysMonths.includes(monthNumber)) { return 31 }
+const calculateMonthDays = (monthNumber, year) => {
+  if (has31Days(monthNumber)) { return 31 }
 
-  if (the30DaysMonths.includes(monthNumber)) { return 30 }
+  if (has30Days(monthNumber)) { return 30 }
 
-  // Check for Leap year
-  if (year % 4 === 0) {
-    if ((year % 100 !== 0) || (year % 100 === 0 && year % 400 === 0)) {
-      return 29
-    }
-  }
+  if (isLeapYear(year)) { return 29 }
 
   return 28
+}
+
+const has31Days = monthNumber => {
+  const the31DaysMonths = [1, 3, 5, 7, 8, 10, 12]
+  return the31DaysMonths.includes(monthNumber)
+}
+
+const has30Days = monthNumber => {
+  const the30DaysMonths = [4, 6, 9, 11]
+  return the30DaysMonths.includes(monthNumber)
+}
+
+const isLeapYear = year => {
+  return ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)
 }
 
 export { getMonthDays }
