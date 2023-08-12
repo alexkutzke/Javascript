0433430faa63d42cebe2a7019d6a00eb016d0e35
Rahula Palu Caleffi <rahulacaleffi@users.noreply.github.com>
2023-08-12 16:35:59 -0300

    Update NextPowerOfTwo.js

--- Bit-Manipulation/NextPowerOfTwo.js
+++ Bit-Manipulation/NextPowerOfTwo.js
@@ -1,18 +1,34 @@
 /**
  *
- *  This script will find next power of two
- *    of given number.
+ *  This script will find the next power of two
+ *  of the given number.
  *  More about it:
  *   https://www.techiedelight.com/round-next-highest-power-2/
  *
  */
 
-export const nextPowerOfTwo = (n) => {
-  if (n > 0 && (n & (n - 1)) === 0) return n
+export const nextPowerOfTwo = (number) => {
+  if (ifHigherThanZero(number) && ifIsPowerOfTwo(number)) return number
+  const result = binaryHandler(number)
+  return result
+}
+
+function ifHigherThanZero(number) {
+  return number > 0
+}
+
+function ifIsPowerOfTwo(number) {
+  return (number & (number - 1)) === 0
+}
+
+function binaryHandler(number) {
+  // starting the variable with 1
   let result = 1
-  while (n > 0) {
+  while (number > 0) {
+    // binary left shift
     result = result << 1
-    n = n >> 1
+    // binary right shift
+    number = number >> 1
   }
   return result
 }
