870ae0a936e2b93df841cec0274c29578c69e453
Andre Klingenfus Antunes <andrekantunes10@gmail.com>
2023-08-13 23:00:21 -0300

    refactor SumOfDigits by adding smaller and meaningful functions

--- Maths/SumOfDigits.js
+++ Maths/SumOfDigits.js
@@ -11,9 +11,8 @@
   NOTE: The final parseInt is just there in cases where 1 digit numbers are given, since without that it would result in a String output.
 */
 function sumOfDigitsUsingString (number) {
-  if (number < 0) number = -number
-
-  return Number.parseInt(number.toString().split('').reduce((a, b) => Number(a) + Number(b)))
+  number = calculateAbsolutValue(number)
+  return splitStringAndSumDigits(number)
 }
 
 /*
@@ -21,26 +20,71 @@ function sumOfDigitsUsingString (number) {
   The sum of all the digits is returned (The res variable acts as a collector, taking the remainders on each iteration)
 */
 function sumOfDigitsUsingLoop (number) {
-  if (number < 0) number = -number
-  let res = 0
-
-  while (number > 0) {
-    res += number % 10
-    number = Math.floor(number / 10)
-  }
-
-  return res
+  number = calculateAbsolutValue(number)
+  return iterateNumberAndSumDigits(number)
 }
 
 /*
   We use the fact that the sum of the digits of a one digit number is itself, and check whether the number is less than 10. If so, then we return the number. Else, we take the number divided by 10 and floored, and recursively call the function, while adding it with the number mod 10
 */
 function sumOfDigitsUsingRecursion (number) {
-  if (number < 0) number = -number
+  number = calculateAbsolutValue(number)
+  if (number < 10) {
+    return number
+  }
 
-  if (number < 10) return number
+  return (
+    getLastDigit(number)) +
+    sumOfDigitsUsingRecursion(removeLastDigit(number))
+}
 
-  return (number % 10) + sumOfDigitsUsingRecursion(Math.floor(number / 10))
+function calculateAbsolutValue (number) {
+  if (isNegative(number)) {
+    number = changeSignal(number)
+  }
+  return number
+}
+
+function isNegative (number) {
+  return number < 0
+}
+
+function changeSignal (number) {
+  return -number
+}
+
+function splitStringAndSumDigits (number) {
+  const digits = splitStringInDigits(number)
+  return sumDigits(digits)
+}
+
+function splitStringInDigits (string) {
+  return string.toString().split('')
+}
+
+function sumDigits (digits) {
+  let total = 0
+  for (const digit of digits) {
+    total += Number(digit)
+  }
+  return total
+}
+
+function iterateNumberAndSumDigits (number) {
+  let total = 0
+  while (number > 0) {
+    total += getLastDigit(number)
+    number = removeLastDigit(number)
+  }
+  return total
+}
+
+function getLastDigit (number) {
+  return number % 10
+}
+
+function removeLastDigit (number) {
+  return Math.floor (number / 10)
 }
 
 export { sumOfDigitsUsingRecursion, sumOfDigitsUsingLoop, sumOfDigitsUsingString }
