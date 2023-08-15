6cae8ad56ac2fb888c9981daec67dbcaafe9c0fb
Mateus Lodi <55122850+mmlodi@users.noreply.github.com>
2023-08-15 19:35:39 -0300

    Refatoração da nomenclatura das funções

--- Maths/BisectionMethod.js
+++ Maths/BisectionMethod.js
@@ -12,34 +12,34 @@
  *
  */
 
-const findRoot = (a, b, func, numberOfIterations) => {
+const findRoot = (firstParameter, secondParameter, mainFunction, numberOfIterations) => {
   // Check if a given  real value belongs to the function's domain
-  const belongsToDomain = (x, f) => {
-    const res = f(x)
-    return !Number.isNaN(res)
+  const belongsToDomain = (anyNumber, anyFunction) => {
+    const result = anyFunction(anyNumber)
+    return !Number.isNaN(result)
   }
-  if (!belongsToDomain(a, func) || !belongsToDomain(b, func)) throw Error("Given interval is not a valid subset of function's domain")
+  if (!belongsToDomain(firstParameter, mainFunction) || !belongsToDomain(secondParameter, mainFunction)) throw Error("Given interval is not a valid subset of function's domain")
 
   // Bolzano theorem
-  const hasRoot = (a, b, func) => {
-    return func(a) * func(b) < 0
+  const hasRoot = (firstParameter, secondParameter, mainFunction) => {
+    return mainFunction(firstParameter) * mainFunction(secondParameter) < 0
   }
-  if (hasRoot(a, b, func) === false) { throw Error('Product f(a)*f(b) has to be negative so that Bolzano theorem is applied') }
+  if (hasRoot(firstParameter, secondParameter, mainFunction) === false) { throw Error('Product f(a)*f(b) has to be negative so that Bolzano theorem is applied') }
 
   // Declare m
-  const m = (a + b) / 2
+  const meanParameter = (firstParameter + secondParameter) / 2
 
   // Recursion terminal condition
-  if (numberOfIterations === 0) { return m }
+  if (numberOfIterations === 0) { return meanParameter }
 
   // Find the products of f(m) and f(a), f(b)
-  const fm = func(m)
-  const prod1 = fm * func(a)
-  const prod2 = fm * func(b)
+  const fm = mainFunction(meanParameter)
+  const productOfFunctionAndA = fm * mainFunction(firstParameter)
+  const productOfFunctionAndB = fm * mainFunction(secondParameter)
 
   // Depending on the sign of the products above, decide which position will m fill (a's or b's)
-  if (prod1 > 0 && prod2 < 0) return findRoot(m, b, func, --numberOfIterations)
-  else if (prod1 < 0 && prod2 > 0) return findRoot(a, m, func, --numberOfIterations)
+  if (productOfFunctionAndA > 0 && productOfFunctionAndB < 0) return findRoot(meanParameter, secondParameter, func, --numberOfIterations)
+  else if (productOfFunctionAndA < 0 && productOfFunctionAndB > 0) return findRoot(firstParameter, meanParameter, func, --numberOfIterations)
   else throw Error('Unexpected behavior')
 }
 
