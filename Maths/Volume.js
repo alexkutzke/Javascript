0a748177f89357a0f793b3856b43727995d418f4
RudhyDev <rudhy.pereira@gmail.com>
2023-08-16 15:31:15 -0300

    refactor: :wastebasket: refactoring clean code

--- Maths/Volume.js
+++ Maths/Volume.js
@@ -1,123 +1,56 @@
-/*
-Calculate the volume of the shapes
-
-Volume for Cuboid
-Volume for Cube
-Volume for Cone
-Volume for Pyramid
-Volume for Cylinder
-Volume for Triangular Prism
-Volume for Pentagonal Prism
-Volume for Sphere
-Volume for Hemisphere
-*/
-
-/*
-  Calculate the volume for a Cuboid
-  Reference: https://www.cuemath.com/measurement/volume-of-cuboid/
-  return width * length * height
-*/
-const volCuboid = (width, length, height) => {
-  isNumber(width, 'Width')
-  isNumber(length, 'Length')
-  isNumber(height, 'Height')
-  return (width * length * height)
-}
-
-/*
-  Calculate the volume for a Cube
-  Reference: https://www.cuemath.com/measurement/volume-of-cube/
-  return length * length * length
-*/
-const volCube = (length) => {
-  isNumber(length, 'Length')
-  return (length ** 3)
-}
-
-/*
-  Calculate the volume for a Cone
-  Reference: https://www.cuemath.com/measurement/volume-of-cone/
-  return PI * radius^2 * height/3
-*/
-const volCone = (radius, height) => {
-  isNumber(radius, 'Radius')
-  isNumber(height, 'Height')
-  return (Math.PI * radius ** 2 * height / 3.0)
-}
-
-/*
-  Calculate the volume for a Pyramid
-  Reference: https://www.cuemath.com/measurement/volume-of-pyramid/
-  return (baseLength * baseWidth * height) / 3
-*/
-const volPyramid = (baseLength, baseWidth, height) => {
-  isNumber(baseLength, 'BaseLength')
-  isNumber(baseWidth, 'BaseWidth')
-  isNumber(height, 'Height')
-  return (baseLength * baseWidth * height) / 3.0
-}
-
-/*
-  Calculate the volume for a Cylinder
-  Reference: https://www.cuemath.com/measurement/volume-of-cylinder/
-  return PI * radius^2 * height
-*/
-const volCylinder = (radius, height) => {
-  isNumber(radius, 'Radius')
-  isNumber(height, 'Height')
-  return (Math.PI * radius ** 2 * height)
-}
-
-/*
-  Calculate the volume for a Triangular Prism
-  Reference: http://lrd.kangan.edu.au/numbers/content/03_volume/04_page.htm
-  return 1 / 2 * baseLengthTriangle * heightTriangle * height
-*/
-const volTriangularPrism = (baseLengthTriangle, heightTriangle, height) => {
-  isNumber(baseLengthTriangle, 'BaseLengthTriangle')
-  isNumber(heightTriangle, 'HeightTriangle')
-  isNumber(height, 'Height')
-  return (1 / 2 * baseLengthTriangle * heightTriangle * height)
-}
-
-/*
-  Calculate the volume for a Pentagonal Prism
-  Reference: https://www.cuemath.com/measurement/volume-of-pentagonal-prism/
-  return 5/2 * pentagonalLength * pentagonalBaseLength * height
-*/
-const volPentagonalPrism = (pentagonalLength, pentagonalBaseLength, height) => {
-  isNumber(pentagonalLength, 'PentagonalLength')
-  isNumber(pentagonalBaseLength, 'PentagonalBaseLength')
-  isNumber(height, 'Height')
-  return (5 / 2 * pentagonalLength * pentagonalBaseLength * height)
-}
-
-/*
-  Calculate the volume for a Sphere
-  Reference: https://www.cuemath.com/measurement/volume-of-sphere/
-  return 4/3 * PI * radius^3
-*/
-const volSphere = (radius) => {
-  isNumber(radius, 'Radius')
-  return (4 / 3 * Math.PI * radius ** 3)
-}
-
-/*
-  Calculate the volume for a Hemisphere
-  Reference: https://www.cuemath.com/measurement/volume-of-hemisphere/
-  return (2 * PI * radius^3)/3
-*/
-const volHemisphere = (radius) => {
-  isNumber(radius, 'Radius')
-  return (2.0 * Math.PI * radius ** 3) / 3.0
-}
-
-const isNumber = (number, noName = 'number') => {
-  if (typeof number !== 'number') {
-    throw new TypeError('The ' + noName + ' should be Number type')
-  } else if (number < 0 || (!Number.isFinite(number))) {
-    throw new Error('The ' + noName + ' only accepts positive values')
+const validatePositiveNumber = (value, name) => {
+  if (typeof value !== 'number') {
+    throw new TypeError(`The ${name} should be of type Number`);
+  } else if (value < 0 || (!Number.isFinite(value))) {
+    throw new Error(`The ${name} only accepts positive values`);
   }
-}
+};
 
-export { volCuboid, volCube, volCone, volPyramid, volCylinder, volTriangularPrism, volPentagonalPrism, volSphere, volHemisphere }
+const VolumeCalculators = {
+  cuboid: (width, length, height) => {
+    [width, length, height].forEach((value, i) => validatePositiveNumber(value, ['Width', 'Length', 'Height'][i]));
+    return width * length * height;
+  },
+  cube: (length) => {
+    validatePositiveNumber(length, 'Length');
+    return length ** 3;
+  },
+  cone: (radius, height) => {
+    validatePositiveNumber(radius, 'Radius');
+    validatePositiveNumber(height, 'Height');
+    return Math.PI * radius ** 2 * height / 3.0;
+  },
+  pyramid: (baseLength, baseWidth, height) => {
+    validatePositiveNumber(baseLength, 'BaseLength');
+    validatePositiveNumber(baseWidth, 'BaseWidth');
+    validatePositiveNumber(height, 'Height');
+    return (baseLength * baseWidth * height) / 3.0;
+  },
+  cylinder: (radius, height) => {
+    validatePositiveNumber(radius, 'Radius');
+    validatePositiveNumber(height, 'Height');
+    return Math.PI * radius ** 2 * height;
+  },
+  triangularPrism: (baseLengthTriangle, heightTriangle, height) => {
+    validatePositiveNumber(baseLengthTriangle, 'BaseLengthTriangle');
+    validatePositiveNumber(heightTriangle, 'HeightTriangle');
+    validatePositiveNumber(height, 'Height');
+    return 0.5 * baseLengthTriangle * heightTriangle * height;
+  },
+  pentagonalPrism: (pentagonalLength, pentagonalBaseLength, height) => {
+    validatePositiveNumber(pentagonalLength, 'PentagonalLength');
+    validatePositiveNumber(pentagonalBaseLength, 'PentagonalBaseLength');
+    validatePositiveNumber(height, 'Height');
+    return (5 / 2 * pentagonalLength * pentagonalBaseLength * height);
+  },
+  sphere: (radius) => {
+    validatePositiveNumber(radius, 'Radius');
+    return (4 / 3 * Math.PI * radius ** 3);
+  },
+  hemisphere: (radius) => {
+    validatePositiveNumber(radius, 'Radius');
+    return (2 * Math.PI * radius ** 3) / 3;
+  }
+};
+
+export { VolumeCalculators };
