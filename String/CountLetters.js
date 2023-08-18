aee5c2e506b26c07f84b4b96e056be88fca1d319
Juniorrek <juniorrek@hotmail.com>
2023-08-18 17:11:29 -0300

    Refatoração atividade 2

--- String/CountLetters.js
+++ String/CountLetters.js
@@ -1,33 +1,56 @@
 /**
  * @function countLetters
  * @description Given a string, count the number of each letter.
- * @param {String} str - The input string
+ * @param {String} string - The input string
  * @return {Object} - Object with letters and number of times
  * @example countLetters("hello") => {h: 1, e: 1, l: 2, o: 1}
  */
 
-const countLetters = (str) => {
+const countLetters = (string) => {
+  validateInput(string);
+
+  const lettersTimes = countLettersOfString(string);
+
+  return lettersTimes;
+}
+
+function countLettersOfString(string) {
+  const lettersTimes = {}
+
+  for (let i = 0; i < string.toLowerCase().length; i++) {
+    const char = string.toLowerCase().charAt(i)
+    lettersTimes[char] = (lettersTimes[char] || 0) + 1
+  }
+
+  return lettersTimes;
+}
+
+function validateInput(string) {
+  validateInputString(string);
+  validateInputSpecialCharacters(string);
+  validateInputNumbers(string);
+}
+
+function validateInputString(string) {
+  if (typeof string !== 'string') {
+    throw new TypeError('Input should be a string');
+  }
+}
+
+function validateInputSpecialCharacters(string) {
   const specialChars = /\W/g
 
-  if (typeof str !== 'string') {
-    throw new TypeError('Input should be a string')
+  if (specialChars.test(string)) {
+    throw new TypeError('Input must not contain special characters');
   }
-
-  if (specialChars.test(str)) {
-    throw new TypeError('Input must not contain special characters')
-  }
-
-  if (/\d/.test(str)) {
-    throw new TypeError('Input must not contain numbers')
-  }
-
-  const obj = {}
-  for (let i = 0; i < str.toLowerCase().length; i++) {
-    const char = str.toLowerCase().charAt(i)
-    obj[char] = (obj[char] || 0) + 1
-  }
-
-  return obj
 }
+
+function validateInputNumbers(string) {
+  if (/\d/.test(string)) {
+    throw new TypeError('Input must not contain numbers');
+  }
+}
+
+console.log(countLetters("hello"));
 
 export { countLetters }
