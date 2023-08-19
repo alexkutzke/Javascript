7c7d73759d3e09f08a3aed745d3ab7daffee8213
julianocba <142115654+julianocba@users.noreply.github.com>
2023-08-18 22:36:55 -0300

    Atividade 2

--- String/ReverseWords.js
+++ String/ReverseWords.js
@@ -1,17 +1,48 @@
 /**
- * @function reverseWords
- * @param {string} str
- * @returns {string} - reverse string
+ * Valida os valores do input.
+ * 
+ * @param {string} str - String do input.
+ * @param {string} letter - Letra escolhida.
+ * @throws {Error} - Caso os inputs sejam inválidos ou tenha mais ou menos de uma letra.
  */
-const reverseWords = (str) => {
-  if (typeof str !== 'string') {
-    throw new TypeError('The given value is not a string')
+const validateInputs = (str, letter) => {
+  if (typeof str !== 'string' || typeof letter !== 'string' || letter.length !== 1) {
+    throw new Error('Os inputs devem ser strings, e a letra deve ser única.');
   }
-
-  return str
-    .split(/\s+/) // create an array with each word in string
-    .reduceRight((reverseStr, word) => `${reverseStr} ${word}`, '') // traverse the array from last & create an string
-    .trim() // remove the first useless space
 }
 
-export default reverseWords
+/**
+ * Conta as ocorrências da letra na string.
+ * 
+ * @param {string} str - String do input.
+ * @param {string} letter - Letra escolhida.
+ * @returns {number} - Quantidade de letras na string.
+ */
+const countLetterOccurrences = (str, letter) => {
+  let count = 0;
+  const targetLetter = letter.toLowerCase();
+  
+  for (const char of str) {
+    if (char.toLowerCase() === targetLetter) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+/**
+ * Retorna o percentual de letras dentro da string arredondado ao precentual mais próximo.
+ * 
+ * @param {string} str - String fornecida.
+ * @param {string} letter - Letra escolhida.
+ * @returns {number} - Percentual de letras em relação a string.
+ * 
+ */
+const percentageOfLetter = (str, letter) => {
+  validateInputs(str, letter);
+  const letterCount = countLetterOccurrences(str, letter);
+  return Math.floor((100 * letterCount) / str.length);
+}
+
+export { percentageOfLetter };
